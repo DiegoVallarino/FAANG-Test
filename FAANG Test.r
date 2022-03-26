79c8b60b6df82e99b85c0680fee73193f3b5c4f8
Diego Vallarino <69391510+DiegoVallarino@users.noreply.github.com>
2022-03-26 10:45:18 +0100

    Update FAANG Test.r

--- FAANG Test.r
+++ FAANG Test.r
@@ -1,5 +1,5 @@
 #These are some of the exercises that the Tech companies put to evaluate the DS with +18 years of experience.
-#In my opinion, the value of these roles goes elsewhere.
+#In my opinion, valueless.
 
 x<-seq(1:10)
 faangTest <- function (x) {
